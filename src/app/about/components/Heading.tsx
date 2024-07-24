68ded4b913077028033f4c5f063d700902dcc02e
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-07-24 23:18:14 +0530

    Create Heading.tsx

---
+++ src/app/about/components/Heading.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+
+function Heading() {
+  return (
+    <div className="border border-gray-700 p-6 rounded-lg shadow-md max-w-md mx-auto mt-40">
+      <h1 className="text-4xl font-bold text-white text-center">About Us</h1>
+    </div>
+  );
+}
+
+export default Heading;
+
+
